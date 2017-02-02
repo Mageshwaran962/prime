10f0f339b33702a549945876535c633ecbb746bd
Mageshwaran962 <mageshwaran962@gmail.com>
2017-02-02 01:09:46 -0800

    prime

---
+++ prime.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+using namespace std;
+ 
+
+int main() 
+{
+ 
+int num1,num2,n;
+ 
+int count=0;
+ cin>>num1;
+
+ cin>>num2;
+ 
+for(int i=num1;i<num2;i++)
+
+ {
+        
+count=0;
+     
+for(int j=2;j<i;j++)
+    
+ {
+        
+if(i%j==0)
+       
+ {
+         
+count++;
+        
+ break;
+        
+ }
+    
+ }
+    
+if(count==0)
+  
+  {
+    
+ cout<<i<<" ";
+  
+  }
+ }
+}
